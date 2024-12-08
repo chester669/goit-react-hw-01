e4b5593d6826d215e0b87b3f01355f0a9d107f67
chester669 <viktorostash44@gmail.com>
2024-12-08 19:19:58 +0200

    homeworks

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+import FriendListItem from "../FriendListItem/FriendListItem";
+import styles from "./FriendList.module.css";
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul className={styles.friendList}>
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <li key={id} className={styles.friendListItem}>
+          <FriendListItem avatar={avatar} name={name} isOnline={isOnline} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+
+export default FriendList;
---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./FriendListItem.module.css";
+
+const FriendListItem = ({ avatar, name, isOnline }) => (
+  <div className={styles.friendListItem}>
+    <img src={avatar} alt={`${name}'s avatar`} className={styles.avatar} />
+    <p className={styles.name}>{name}</p>
+    <p className={isOnline ? styles.online : styles.offline}>
+      {isOnline ? "Online" : "Offline"}
+    </p>
+  </div>
+);
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
+
+export default FriendListItem;
